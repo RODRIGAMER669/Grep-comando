d3f1ff1ea19666c381c71701fb8e2380f4766948
RODRIGAMER669 <alexis.rr0501@gmail.com>
2025-10-16 16:31:06 -0500

    codigo funcionable

--- demo/src/main/java/com/example/proceso/grep/Lanzador.java
+++ demo/src/main/java/com/example/proceso/grep/Lanzador.java
@@ -3,38 +3,46 @@ package com.example.proceso.grep;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 public class Lanzador {
 
-    // Importante: incluir el classpath (.) para que java encuentre la clase Escritor
-    final static String[] COMANDOS = {"java", "com.example.proceso.grep.Escritor"};
+    final static String[] COMANDOS = {"grep", "PSP"};
+    public static final String MSG_ERROR = "Se ha producido un error al ejecutar el comando";
+
+    public static final String MSG = "Me gusta PSP y java\r\n"
+            + "PSP se programa en java\r\n"
+            + "es un módulo de DAM\r\n"
+            + "y se programa de forma concurrente en PSP\r\n"
+            + "PSP es programación.";
 
     public static void main(String[] args) {
         try {
             Process p = Runtime.getRuntime().exec(COMANDOS);
-
-            try (PrintWriter pw = new PrintWriter(new OutputStreamWriter(p.getOutputStream()), true)) {
-
-                pw.println("Me gusta PSP y java");
-                pw.println("PSP se programa en java");
-                pw.println("es un módulo de DAM");
-                pw.println("y se programa de forma concurrente en PSP");
-                pw.println("PSP es programación.");
-                pw.close();
-
+            StringBuilder sb = new StringBuilder();
+            OutputStream out = p.getOutputStream();
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
+            pw.println(MSG);
+            pw.close();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
             }
-            p.waitFor();
-            try (BufferedReader salida = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
-                String linea;
-                while ((linea = salida.readLine()) != null) {
-                    System.out.println(linea);
-                }
+
+            int exitVal = p.waitFor();
+            if (exitVal == 0) {
+                System.out.println(sb);
+                System.exit(0);
+            } else {
+                System.out.println(MSG_ERROR);
+                System.exit(1);
             }
 
         } catch (IOException | InterruptedException e) {
-            e.getMessage();
+            System.exit(34);
         }
     }
 }
